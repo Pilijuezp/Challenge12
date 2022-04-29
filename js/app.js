2102f0462bc14a135973bd72d2de1c396120e089
Pilar Juez Perez <pilijuezp@gmail.com>
2022-04-29 14:27:47 -0300

    primer commit challenge12

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let base= 50;
+let height=35;
+let triangleArea= (base*height%2);
+console.log(triangleArea);
+
+let radio=45;
+let circleArea=(Math.PI*(Math.pow(radio,2)));
+console.log(circleArea);
+
